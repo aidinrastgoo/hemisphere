789b2230252d5da6ce87c10df6b30501cf42e44d
Aidin Rastgoo <aidinrastgoo@gmail.com>
2023-11-10 17:45:22 +0100

    defineABM

--- src/hemisphere.jl
+++ src/hemisphere.jl
@@ -5,6 +5,7 @@ Pkg.add("SpecialFunctions")
 
 include("A.jl")
 include("M.jl")
+include("B.jl")
 
 
 export calculate_A
@@ -12,5 +13,7 @@ export A_kn
 export create_matrix
 export η_k
 export generate_U_matrix
+export calculate_M
+export calculate_B
 
 end # module hemisphere
