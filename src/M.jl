872b1bbd63c39a1ee6e37b41eb2ff7f7465b924a
Aidin Rastgoo <aidinrastgoo@gmail.com>
2023-11-10 16:50:18 +0100

    Programm_update_1.0

--- src/M.jl
+++ src/M.jl
@@ -11,11 +11,37 @@ function create_matrix(n, k)
         return 1.0 / (2n + 1)
     elseif (n + k) % 2 == 0
         return 0.0
+    elseif n == 0 && k >= 1
+        t1 = cos(π / (2k)) * (1 / A_kn(k, n))
+        t2 = -k * (k + 1)
+        t3 = sin(π / (2k)) * A_kn(k, n)
+        return (2 / π) * ((t1 / t2) - (t3 / t2))
+    elseif k == 0 && n >= 1
+        t11 = sin(π / (2n)) * (1 / A_kn(k, n))
+        t12 = n * (n + 1)
+        t13 = cos(π / (2n)) * A_kn(k, n)
+        return (2 / π) * ((t11 / t12) - (t13 / t12))
     else
-        A_nk = 1/A_kn(k, n)
-        return (2 / π) * ((sin(π / (2n)) * cos(π / (2k)) * A_nk) / (n * (n + 1) - k * (k + 1)) - (sin(π / (2k)) * cos(π / (2n)) * (1/A_nk)) / (n * (n + 1) - k * (k + 1)))
+        t21 = (sin(π / (2n)) * cos(π / (2k))) / A_kn(k, n)
+        t22 = n * (n + 1) - k * (k + 1)
+        t23 = sin(π / (2k)) * cos(π / (2n)) * A_kn(k, n)
+        return (2 / π) * ((t21 / t22) - (t23 / t22))
     end
 end
+
+
+
+function generate_U_matrix(n_max, k_max)
+    U = zeros(Float64, n_max, k_max)
+    for n in 0:n_max-1
+        for k in 0:k_max-1
+            U[n+1, k+1] = create_matrix(n, k)
+        end
+    end
+    return U
+end
+
+
 
 function η_k(k, ε_r1, ε_r2)
     if k % 2 == 0
@@ -25,32 +51,21 @@ function η_k(k, ε_r1, ε_r2)
     end
 end
 
-
-function generate_U_matrix(n_max, k_max)
-    U = zeros(Float64, n_max, k_max)
-    for n in 1:n_max
-        for k in 1:k_max
-            U[n, k] = create_matrix(n, k)
-        end
-    end
-    return U
-end
-
-function calculate_M(a, ε_r1, ε_r2, n_max, k_max)
+function calculate_M(radius, ε_r1, ε_r2, n_max, k_max)
     U = generate_U_matrix(n_max, k_max)
     M = zeros(Float64, n_max, k_max)
     
-    for n in 1:n_max
-        for k in 1:k_max
+    for n in 0:n_max-1
+        for k in 0:k_max-1
             η_k_val = η_k(k, ε_r1, ε_r2)
             term1 = η_k_val * (n + 1)
             term2 = η_k_val * k * ε_r1
-            term3 = (-1)^(n + k) * (n + 1)
-            term4 = (-1)^(n + k) * k * ε_r2
-            M[n, k] = a^-(n + 2) * (term1 + term2 + term3 + term4) * U[n, k]
+            term3 = ((-1)^(n + k)) * (n + 1)
+            term4 = ((-1)^(n + k)) * k * ε_r2
+            M[n+1, k+1] = (radius^(-(n + 2))) * (term1 + term2 + term3 + term4) * U[n+1, k+1]
         end
     end
-    return M
+    return @show M
 end
 
 
--- test/testU.jl
+++ test/testU.jl
@@ -1,16 +1,14 @@
 Amplitude = 1.0
 ε_r1 = 1.0
 ε_r2 = 1.0
-n_max = 6
-k_max = 6
+n_max = 10
+k_max = 10
 radius = 1.0  
-
+U = generate_U_matrix(n_max, k_max)
 A = calculate_A(Amplitude, ε_r1, ε_r2, k_max)
 M = calculate_M(radius, ε_r1, ε_r2, n_max, k_max)  # Calculate the M matrix
-B = A \ M  
+@show B = inv(M)*A  
 
 
 
-
-
-@test sum(B)==3.757952410879924
+@test sum(B) == 0
