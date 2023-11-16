516a07893f249376a676b94f4321122a87ff512d
Aidin Rastgoo <aidinrastgoo@gmail.com>
2023-11-16 09:07:07 +0100

    Test mismatch error

--- test/testA.jl
+++ test/testA.jl
@@ -1,6 +1,8 @@
-Amplitude = 99999999999999999999999.0
-ε_r1 = 1.0
-ε_r2 = 1.0
-k_max = 342
-A = calculate_A(Amplitude, ε_r1, ε_r2, k_max)
-@test sum(A) == 0
+Amplitude = rand()
+ε_r1 = ε_r2 = 1.0
+k_max =n_max= rand(10:342)
+radius = rand()
+B_h = calculate_B(Amplitude, radius, ε_r1, ε_r2, n_max, k_max)
+A_h = calculate_A(Amplitude, ε_r1, ε_r2, k_max)
+
+@test sum(B_h) + sum(A_h) == 0  
--- test/testB.jl
+++ test/testB.jl
@@ -1,8 +1,18 @@
-Amplitude = 99999999999999999999999.0
-ε_r1 = 1.0
-ε_r2 = 1.0
-n_max = 342 
-k_max = 342
-radius = 7.9  
-B = calculate_B(Amplitude, radius, ε_r1, ε_r2, n_max, k_max)
-@test sum(B) == 0
+Amplitude = rand()
+ε_r1 = rand()
+ε_r2 = ε_r1
+n_max = k_max = rand(10:342)
+radius = rand()
+
+B_h = calculate_B(Amplitude, radius, ε_r1, ε_r2, n_max, k_max)
+B_s =calculate_BSphere(Amplitude, radius, ε_r1, n_max, k_max)
+
+
+function euclidean_distance(vec1, vec2)
+    return sqrt(sum((vec1 .- vec2) .^ 2))
+end
+
+distance = euclidean_distance(B_h, B_s)
+@show distance
+
+@test distance < 1.0e-16
